6da675621780fc85906070a0ae9c47384c91bd2c
huongnt <huongnt9820@gmail.com>
2022-02-15 20:31:04 +0700

    delete comment

--- lib/main.dart
+++ lib/main.dart
@@ -37,14 +37,11 @@ class _MyHomePageState extends State<MyHomePage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
         title: const Text('Movies'),
       ),
       body: GetBuilder<DataController>(
           init: DataController(),
           builder: (controller) {
-            print('item-----------------${controller.items}');
             if (controller.items == null) {
               return const Center(
                 child: CircularProgressIndicator(),
